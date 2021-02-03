8c4400ea6e10f20041e5fbf69cf98ee0045ab77f
Zholkenova Zhansaya <190103046@stu.sdu.edu.kz>
2021-02-04 02:09:32 +0600

    added first php

--- first.php
+++ first.php
@@ -3,7 +3,7 @@
 <body>
 
 <?php
-echo "You can't go back to the past, which means you can't live in it. You can't get into the future, which means you can't hope for it. You think that the future will come tomorrow, but when you wake up tomorrow morning, you will be back in the present";
+echo "You can't go back to the past, which means you can't live in it. You can't get into the future, which means you can't hope for it. You think that the future will come tomorrow, but when you wake up tomorrow morning, you will be back in the present...";
 ?>
 
 </body>
