0905ff76108211bbab211af0c64202534375f612
Zholkenova Zhansaya <190103046@stu.sdu.edu.kz>
2021-02-04 01:37:58 +0600

    created first php

---
+++ first.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "You can't go back to the past, which means you can't live in it. You can't get into the future, which means you can't hope for it. You think that the future will come tomorrow, but when you wake up tomorrow morning, you will be back in the present";
+?>
+
+</body>
+</html>
