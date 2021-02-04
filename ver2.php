3909c95436aec5f507b8555540e0c17a6ffd5418
Zholkenova Zhansaya <190103046@stu.sdu.edu.kz>
2021-02-04 12:38:42 +0600

    instructions

--- ver2.php
+++ ver2.php
@@ -5,7 +5,8 @@
 </head>
 <body>
 	echo "<h2>GIT is LIT!</h2>";
-    
+    echo "To change this file and commit it we should use get status to see local changes that git add . then do commit with description then pull it after that push it to github<br>";
+
 
 </body>
 </html>
