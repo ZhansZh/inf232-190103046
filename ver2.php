de539725c4e703191dc77cc1be091d2c6531cc5c
Zholkenova Zhansaya <190103046@stu.sdu.edu.kz>
2021-02-04 12:51:21 +0600

    last change

--- ver2.php
+++ ver2.php
@@ -5,7 +5,9 @@
 </head>
 <body>
 	echo "<h2>GIT is LIT!</h2>";
-    echo "To change this file and commit it we should use get status to see local changes that git add . then do commit with description then pull it after that push it to github<br>";
+    echo "To change this file and commit it we should use get status to see local changes then git add . then do commit with description then pull it after that push it to github<br>";
+    echo "Third change of the file";
+    echo "Lets say I added here some code It's my last change"
 
 
 </body>
